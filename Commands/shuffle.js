8fd64bb4c0a3ee2f810b8728c24f6f8763b4a6a4
Adrian Chrysanthou <adrianvfx@gmail.com>
2022-07-09 16:20:38 -0400

    wow work

---
+++ Commands/shuffle.js
@@ -0,0 +1,68 @@
+/** @format */
+
+const Command = require('../Structures/Command.js');
+
+const Discord = require('discord.js');
+
+const request = require('request');
+
+const options = {
+	url: 'https://steam.cma.dk/apps?limit=8&random=1&category=0&genre=0&free=0&format=json',
+	method: 'GET',
+	headers: {
+		'Accept': 'application/json',
+		'Accept-Charset': 'utf-8',
+		'User-Agent': 'wheelhausDiscordBot',
+	},
+};
+
+module.exports = new Command({
+	name: 'shuffle',
+	description: 'Randomized WheelHaus',
+	usage: 'Display Randomized Game from Wheelhaus',
+	type: 'BOTH',
+	slashCommandOptions: [],
+	permission: 'SEND_MESSAGES',
+	async run(message) {
+		const embed = new Discord.MessageEmbed();
+
+		request(options, function(err, response, body) {
+			if (err) {
+				const error = 'cannot connect to the server';
+				message.channel.send(error);
+				message.channel.send('No results, check your spelling first');
+			}
+			else {
+				const wheelhausJSON = JSON.parse(body);
+				for (let i = 0; i < wheelhausJSON.length; i++) {
+					const wheelHausMessage = `${wheelhausJSON[i].name}` + '\n';
+					const wheelHausMessageDesc = `${wheelhausJSON[i].description.replace(/<[^>]*>?/gm, '').slice(0, 500)}` + '\n' + 'Follow this link above to read more';
+					const wheelHausURL = `https://store.steampowered.com/app/${wheelhausJSON[i].id}`;
+					const wheelhausImage = `${wheelhausJSON[i].image}`;
+					const wheelhausPrice = `${wheelhausJSON[i].price}`;
+					embed
+						.setTitle(`${wheelHausMessage}`)
+						.setURL(`${wheelHausURL}`)
+						.setDescription(
+							`${wheelHausMessageDesc}`,
+						)
+						.setColor(16679428)
+						.setThumbnail(`${wheelhausImage}`)
+						.setTimestamp()
+						.setImage(
+							`${wheelhausImage}`,
+						)
+						.setFooter({ text: 'Have questions? Twitter @_ok_adrian' })
+						.addFields (
+							{
+								name: 'Price: ',
+								value: `${wheelhausPrice}`,
+							},
+						),
+					message.reply({ embeds: [embed] });
+					break;
+				}
+			}
+		});
+	},
+});
---
+++ Commands/tags.js
@@ -0,0 +1,66 @@
+/** @format */
+
+const Command = require('../Structures/Command.js');
+
+const Discord = require('discord.js');
+
+const request = require('request');
+
+const options = {
+	url: 'https://steam.cma.dk/tags',
+	method: 'GET',
+	headers: {
+		'Accept': 'application/json',
+		'Accept-Charset': 'utf-8',
+		'User-Agent': 'wheelhausDiscordBot',
+	},
+};
+
+module.exports = new Command({
+	name: 'list_of_tags',
+	description: 'List of all Tags',
+	usage: 'List of all Available TagS',
+	type: 'BOTH',
+	slashCommandOptions: [],
+	permission: 'SEND_MESSAGES',
+	async run(message) {
+		const embed = new Discord.MessageEmbed();
+		function sortByName(array) {
+			return array.sort(function(a, b) {
+				const x = a['name']; const y = b['name'];
+				return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+			});
+		}
+		request(options, function(err, response, body) {
+			if (err) {
+				const error = 'cannot connect to the server';
+				message.channel.send(error);
+				message.channel.send('No results, check your spelling first');
+			}
+			else {
+				const wheelhausJSON = JSON.parse(body);
+				// Sort by Name
+				const sortedJson = sortByName(wheelhausJSON);
+				// Grab just name
+				const tagsName = sortedJson.map(a => a.name);
+				const listTags = JSON.stringify(tagsName);
+				const regexTags = listTags.replace(/^\[|\]$/, '').split(',').join(', ');
+				const regexTagsStripQuotations = regexTags.replace(/"([^"]+(?="))"/g, '$1');
+				const regexString = JSON.stringify(regexTagsStripQuotations);
+				const wheelHausMessage = 'List of all Tags';
+				const wheelHausMessageDesc = `${regexString.slice(0, 2010)}` + '\n' + 'Follow the link above to see more!';
+				const wheelHausURL = 'https://steamdb.info/tags/';
+				embed
+					.setTitle(`${wheelHausMessage}`)
+					.setURL(`${wheelHausURL}`)
+					.setDescription(
+						`${wheelHausMessageDesc}`,
+					)
+					.setColor(16679428)
+					.setTimestamp()
+					.setFooter({ text: 'Have questions? Twitter @_ok_adrian' }),
+				message.reply({ embeds: [embed] });
+			}
+		});
+	},
+});
